c5be8794e05666b5465edc4783acbdf7523401fc
Pranjal Choubey <pranjalchoubey@Pranjals-MacBook-Air.local>
2023-04-01 13:51:48 +0530

    create a mongo client and add connections

---
+++ database/cart.go
@@ -0,0 +1,29 @@
+package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("can't find the cart item")
+	ErrCantDecodeProducts = errors.New("can't decode the cart item")
+	ErrUserIdInvalid      = errors.New("user ID is not invalid")
+	ErrCantUpdateUser     = errors.New("cannot add this product to the cart")
+	ErrCantRemoveCartItem = errors.New("cannot remove this item from the cart")
+	ErrCantGetItem        = errors.New("not able to get the item from the cart")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstanBuyer() {
+
+}
