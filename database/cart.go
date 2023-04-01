e345dc0a25d8bf628ab39029a606bcf27992ceb4
Pranjal Choubey <pranjalch99@gmail.com>
2023-04-01 14:09:46 +0530

    change in error message to check commits

--- database/cart.go
+++ database/cart.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	ErrCantFindProduct    = errors.New("can't find the cart item")
 	ErrCantDecodeProducts = errors.New("can't decode the cart item")
-	ErrUserIdInvalid      = errors.New("user ID is not invalid")
+	ErrUserIdInvalid      = errors.New("the user ID is not invalid")
 	ErrCantUpdateUser     = errors.New("cannot add this product to the cart")
 	ErrCantRemoveCartItem = errors.New("cannot remove this item from the cart")
 	ErrCantGetItem        = errors.New("not able to get the item from the cart")
