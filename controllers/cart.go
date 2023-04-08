859daa0a6e17db24bc165b4072231971648e99b5
Pranjal Choubey <pranjalch99@gmail.com>
2023-04-08 18:31:35 +0530

    added the GetItemsFromCart controller

--- controllers/cart.go
+++ controllers/cart.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/pranjalch99/ecommerce-golang/database"
+	"github.com/pranjalch99/ecommerce-golang/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -108,6 +110,60 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 }
 
 func GetItemFromCart() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error": "invalid query"})
+			c.Abort()
+			return
+		}
+
+		userId, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var filledCart models.User
+
+		err = UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: userId}}).Decode(&filledCart)
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(500, "not found")
+			return
+		}
+
+		//Aggregate function of mongo has 3 stages -> filter matching, unwinding, grouping
+		filter_match := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: userId}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "userCart"}}}}
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$_id"}, {Key: "$total", Value: bson.D{primitive.E{Key: "$sum", Value: "$userCart.price"}}}}}}
+
+		pointCursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+		if err != nil {
+			log.Println(err)
+		}
+
+		var listing []bson.M
+
+		err = pointCursor.All(ctx, &listing)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		for _, json := range listing {
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledCart.UserCart)
+		}
+
+		ctx.Done()
+
+	}
 
 }
 
